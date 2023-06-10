2b67e23b6cf50aeedf818caf15c5f137e36c0aed
Will <wdputnam42@gmail.com>
2023-06-10 16:40:49 -0400

    small typo in the controllers files

--- controllers/userController.js
+++ controllers/userController.js
@@ -104,7 +104,7 @@ module.exports = {
         try {
             const user = await User.findOneAndRemove(
                 { _id: req.params.userId },
-                { $pull: { friends: { _id: req.params.friendId } } },
+                { $pull: { friends: { friendId: req.params.friendId } } },
                 { runValidators: true, new: true }
             );
 
